83fc9f48e6f003275e64961b24c03b463b8e638e
Bakar <nightsave4268@gmail.com>
2023-11-10 13:03:08 +0300

    added changeMenu on profile page

--- js/login/SHESH_bg.js
+++ js/login/SHESH_bg.js
@@ -19,34 +19,34 @@
 
     document.querySelector('body').appendChild(canvas);
 
-    window.onresize = function(){
+    window.onresize = function() {
         w = canvas.width = innerWidth,
         h = canvas.height = innerHeight;        
     }
 
-    class Particle{
-        constructor(){
+    class Particle {
+        constructor() {
             this.x = Math.random()*w;
             this.y = Math.random()*h;
             this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
             this.velocityY = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
             this.life = Math.random()*properties.particleLife*60;
         }
-        position(){
+        position() {
             this.x + this.velocityX > w && this.velocityX > 0 || this.x + this.velocityX < 0 && this.velocityX < 0? this.velocityX*=-1 : this.velocityX;
             this.y + this.velocityY > h && this.velocityY > 0 || this.y + this.velocityY < 0 && this.velocityY < 0? this.velocityY*=-1 : this.velocityY;
             this.x += this.velocityX;
             this.y += this.velocityY;
         }
-        reDraw(){
+        reDraw() {
             ctx.beginPath();
             ctx.arc(this.x, this.y, properties.particleRadius, 0, Math.PI*2);
             ctx.closePath();
             ctx.fillStyle = properties.particleColor;
             ctx.fill();
         }
-        reCalculateLife(){
-            if(this.life < 1){
+        reCalculateLife() {
+            if (this.life < 1) {
                 this.x = Math.random()*w;
                 this.y = Math.random()*h;
                 this.velocityX = Math.random()*(properties.particleMaxVelocity*2)-properties.particleMaxVelocity;
@@ -57,12 +57,12 @@
         }
     }
 
-    function reDrawBackground(){
+    function reDrawBackground() {
         ctx.fillStyle = properties.bgColor;
         ctx.fillRect(0, 0, w, h);
     }
 
-    function drawLines(){
+    function drawLines() {
         var x1, y1, x2, y2, length, opacity;
         for(var i in particles){
             for(var j in particles){
@@ -85,7 +85,7 @@
         }
     }
 
-    function reDrawParticles(){
+    function reDrawParticles() {
         for(var i in particles){
             particles[i].reCalculateLife();
             particles[i].position();
@@ -93,15 +93,15 @@
         }
     }
 
-    function loop(){
+    function loop() {
         reDrawBackground();
         reDrawParticles();
         drawLines();
         requestAnimationFrame(loop);
     }
 
-    function init(){
-        for(var i = 0 ; i < properties.particleCount ; i++){
+    function init() {
+        for (var i = 0 ; i < properties.particleCount ; i++) {
             particles.push(new Particle);
         }
         loop();
--- js/profile/btnsWork.js
+++ js/profile/btnsWork.js
@@ -1,9 +1,16 @@
 (() => {
 
     const backBtn = document.querySelector('.main-nav__descr');
+    const changeBtn = document.querySelector('.main-profile__svg');
+    const changeMenu = document.querySelector('.main-changeMenu');
+
 
     backBtn.addEventListener('click', () => {
         window.location.href = '../home';
+    });
+
+    changeBtn.addEventListener('click', () => {
+        changeMenu.classList.toggle('active');
     });
 
 })();
