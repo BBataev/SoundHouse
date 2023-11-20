b334648e8f41be5e19bcd6f15931f917af18e435
Bakar <nightsave4268@gmail.com>
2023-11-20 17:25:34 +0300

    hash stayed for now, added changing block animation

--- js/main/navChange.js
+++ js/main/navChange.js
@@ -1,62 +1,133 @@
 (() => {
     function removeChoosedClass() {
-        home.classList.remove('choosed');
-        search.classList.remove('choosed');
-        music.classList.remove('choosed');
-        albums.classList.remove('choosed');
-        following.classList.remove('choosed');
+        homeBtn.classList.remove('choosed');
+        searchBtn.classList.remove('choosed');
+        musicBtn.classList.remove('choosed');
+        albumsBtn.classList.remove('choosed');
+        followingBtn.classList.remove('choosed');
+
+        homeBlock.classList.remove('active');
+        searchBlock.classList.remove('active');
+        musicBlock.classList.remove('active');
+        albumsBlock.classList.remove('active');
+        followingBlock.classList.remove('active');
     }
 
-    const home = document.querySelector('.home');
-    const search = document.querySelector('.search');
-    const music = document.querySelector('.music');
-    const albums = document.querySelector('.albums');
-    const following = document.querySelector('.following');
+    function getTranslateValue(element) {
+        var transformValue = window.getComputedStyle(element).getPropertyValue('transform');
+        var matrix = new DOMMatrix(transformValue);
+        return matrix.m41;
+      }
 
-    home.addEventListener('click', () => {
+    const checkin = () => {
+        const translateValueHome = getTranslateValue(homeBlock);
+        const translateValueSearch = getTranslateValue(searchBlock);
+        const translateValueMusic = getTranslateValue(musicBlock);
+        const translateValueAlbums = getTranslateValue(albumsBlock);
+        const translateValueFollow = getTranslateValue(followingBlock);
+
+        if (translateValueHome === 0) {
+            homeBlock.classList.add('scrolled')
+            setTimeout(() => {
+                homeBlock.classList.remove('scrolled')
+            }, 300);
+        };
+
+        if (translateValueSearch === 0) {
+            searchBlock.classList.add('scrolled')
+            setTimeout(() => {
+                searchBlock.classList.remove('scrolled')
+            }, 300);
+        };
+
+        if (translateValueMusic === 0) {
+            musicBlock.classList.add('scrolled')
+            setTimeout(() => {
+                musicBlock.classList.remove('scrolled')
+            }, 300);
+        };
+
+        if (translateValueAlbums === 0) {
+            albumsBlock.classList.add('scrolled')
+            setTimeout(() => {
+                albumsBlock.classList.remove('scrolled')
+            }, 300);
+        };
+
+        if (translateValueFollow === 0) {
+            followingBlock.classList.add('scrolled')
+            setTimeout(() => {
+                followingBlock.classList.remove('scrolled')
+            }, 300);
+        };
+     }
+
+    const homeBtn = document.querySelector('.home');
+    const searchBtn = document.querySelector('.search');
+    const musicBtn = document.querySelector('.music');
+    const albumsBtn = document.querySelector('.albums');
+    const followingBtn = document.querySelector('.following');
+
+    const homeBlock = document.querySelector('.main-info-home');
+    const searchBlock = document.querySelector('.main-info-search');
+    const musicBlock = document.querySelector('.main-info-music');
+    const albumsBlock = document.querySelector('.main-info-albums');
+    const followingBlock = document.querySelector('.main-info-following');
+
+    homeBtn.addEventListener('click', () => {
         removeChoosedClass();
-        home.classList.add('choosed');
+        checkin();
+        homeBtn.classList.add('choosed');
+        homeBlock.classList.add('active');
         updateUrl('home');
     });
 
-    search.addEventListener('click', () => {
+    searchBtn.addEventListener('click', () => {
         removeChoosedClass();
-        search.classList.add('choosed');
+        checkin();
+        searchBtn.classList.add('choosed');
+        searchBlock.classList.add('active');
         updateUrl('search');
     });
 
-    music.addEventListener('click', () => {
+    musicBtn.addEventListener('click', () => {
         removeChoosedClass();
-        music.classList.add('choosed');
+        checkin();
+        musicBtn.classList.add('choosed');
+        musicBlock.classList.add('active');
         updateUrl('music');
     });
 
-    albums.addEventListener('click', () => {
+    albumsBtn.addEventListener('click', () => {
         removeChoosedClass();
-        albums.classList.add('choosed');
+        checkin();
+        albumsBtn.classList.add('choosed');
+        albumsBlock.classList.add('active');
         updateUrl('albums');
     });
 
-    following.addEventListener('click', () => {
+    followingBtn.addEventListener('click', () => {
         removeChoosedClass();
-        following.classList.add('choosed');
+        checkin();
+        followingBtn.classList.add('choosed');
+        followingBlock.classList.add('active');
         updateUrl('following');
     });
 
-    // Функция обновления URL с использованием хэша
     function updateUrl(section) {
         const newUrl = `http://bateev.com/main#${section}`;
         window.history.replaceState({}, document.title, newUrl);
     }
 
-    // Проверка наличия хэша при загрузке страницы
     window.addEventListener('load', () => {
         const hash = window.location.hash.substring(1);
         if (hash) {
-            const selectedElement = document.querySelector(`.${hash}`);
-            if (selectedElement) {
+            const selectedElementBtn = document.querySelector(`.${hash}`);
+            const selectedElementBlock = document.querySelector(`.main-info-${hash}`);
+            if (selectedElementBtn) {
                 removeChoosedClass();
-                selectedElement.classList.add('choosed');
+                selectedElementBtn.classList.add('choosed');
+                selectedElementBlock.classList.add('active');
             }
         }
     });
